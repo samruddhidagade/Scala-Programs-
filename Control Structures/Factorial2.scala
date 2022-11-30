837c5c1bc48acf4b84e5dd1bf7dfc10268abe0bc
samruddhidagade <93899330+samruddhidagade@users.noreply.github.com>
2022-12-01 00:12:45 +0530

    Update Factorial2.scala

--- Control Structures/Factorial2.scala
+++ Control Structures/Factorial2.scala
@@ -1,5 +1,5 @@
 //Q2) Write a program to calculate a factorial of a number
-object HelloWorld {
+object Factorial2{
 	def main(args: Array[String]): Unit = {
 	  var n = scala.io.StdIn.readInt();
 	  println("Enter the number :--> ");
